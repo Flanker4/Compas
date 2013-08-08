46ff18cd343cea2b5d8eeed73686667bc00d8084
Flanker4 <_flanker_@mail.ru>
2013-08-08 20:09:58 +0300

    added core location support

--- Compas/Compas/LLViewController.h
+++ Compas/Compas/LLViewController.h
@@ -7,7 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface LLViewController : UIViewController
+@interface LLViewController : UIViewController<CLLocationManagerDelegate>
 @property (nonatomic) CGFloat arrowAngle;
+@property (nonatomic,strong) CLLocationManager * locationManager;
 @end
--- Compas/Compas/LLViewController.m
+++ Compas/Compas/LLViewController.m
@@ -24,13 +24,22 @@ static double angle[] = {M_PI_4,M_PI_2,M_PI,M_PI_4,3*M_PI_4};
 - (void)viewDidLoad{
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self locationManager];
 }
 
 -(void) viewWillAppear:(BOOL)animated{
     //self.arrowAngle = M_PI;
 }
+
+-(NSUInteger)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskPortrait;
+}
+//
+// MARK: - Setters|Getters
+//
+
 -(void) setArrowAngle:(CGFloat)arrowAngle{
-    CABasicAnimation* rotationAnimation = nil;
+    /*CABasicAnimation* rotationAnimation = nil;
     rotationAnimation = (CABasicAnimation*)[_compasArrow.layer animationForKey:@"transform.rotation.z"];
     CGFloat currentAngle = 0;
     if (rotationAnimation!=nil) {
@@ -54,14 +63,39 @@ static double angle[] = {M_PI_4,M_PI_2,M_PI,M_PI_4,3*M_PI_4};
     rotationAnimation.fillMode = kCAFillModeForwards;
     rotationAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
 
-    [_compasArrow.layer addAnimation:rotationAnimation forKey:@"transform.rotation.z"];
-    
+    [_compasArrow.layer addAnimation:rotationAnimation forKey:@"transform.rotation.z"];*/
+    _compasArrow.transform = CGAffineTransformMakeRotation(arrowAngle);
     _arrowAngle=arrowAngle;
 }
 
--(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-    [super touchesEnded:touches withEvent:event];
 
-    self.arrowAngle=angle[i++];
+-(CLLocationManager*) locationManager{
+    if (_locationManager) {
+        return _locationManager;
+    }
+    if ([CLLocationManager headingAvailable]==NO) {
+        return nil;
+    }
+    _locationManager = [CLLocationManager new];
+    _locationManager.delegate = self;
+    _locationManager.distanceFilter=500;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    _locationManager.headingFilter = 5;
+    [_locationManager startUpdatingHeading];
+    return _locationManager;
+}
+
+
+
+//
+// MARK: - location delegate
+//
+-(void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
+    if (newHeading.headingAccuracy<0) {
+        return;
+    }
+    CLLocationDirection theHeading = ((newHeading.trueHeading>0)?newHeading.trueHeading:newHeading.magneticHeading);
+    NSLog(@"Heading = %f",theHeading);
+    self.arrowAngle=-(((double)theHeading)*M_PI)/180.0f;
 }
 @end
